f5d35f99726e31729c7cf43243bfdb5d37f5140f
James Marchant <marchant.jm@googlemail.com>
2020-04-27 18:15:10 +0100

    Replace hardcoded 10 with a function that calculates a rough upper bound for the root

--- dnd_demographics.py
+++ dnd_demographics.py
@@ -6,6 +6,8 @@ from typing import Dict
 
 from scipy import optimize
 
+_MAX_ITER = 1000
+
 ONE_MILLION = 1_000_000
 NUM_LEVELS = 20 + 1  # for level "0"
 
@@ -41,14 +43,34 @@ def calc_geometric_ratio(target_sum: int, num_levels: int = NUM_LEVELS) -> float
     """
     # r = 1 represents a trivial solution so the base must be larger than that
     # r = 10 gives a target sum of 1.11E20 (for 21 levels) so unlikely we'll ever be looking that high!
-    lower = 1.+sys.float_info.epsilon
-    upper = 10
+    lower = 1. + sys.float_info.epsilon
+    upper = _get_rough_upper(target_sum, num_levels)
 
     solution = optimize.root_scalar(ratio_formula, args=(target_sum,), bracket=[lower, upper])
     if solution.converged:
         return solution.root
     else:
         raise ValueError(f"Cannot find acceptable ratio for target sum of {target_sum} with {num_levels} levels.")
+
+
+def _get_rough_upper(target_sum: int, num_levels: int):
+    """Calculate a rough upper bound for the ratio by simply running through the numbers"""
+    if num_levels <= 1:
+        raise ValueError("Number of levels must be greater than 1 for geometric series to work.")
+
+    # Start at 2 as 1 is invalid
+    upper = 2
+    while True:
+        if _geo_sum(upper, num_levels) > target_sum:
+            return upper
+        elif upper > _MAX_ITER:
+            raise ValueError(f"Unable to find appropriate upper bound before reaching {_MAX_ITER=} iterations.")
+        upper += 1
+
+
+def _geo_sum(r: int, n: int):
+    """Calculate the geometric sum for ratio, r, and number of terms, n."""
+    return (1 - r ** n) / (1 - r)
 
 
 def ratio_formula(x: float, target_sum: int, num_levels: int = NUM_LEVELS) -> float:
